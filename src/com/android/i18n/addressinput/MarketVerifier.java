37d23243ae76f8ab74c9939107c8ab958a65bc8e
Jeanine Lilleng <jeanine@google.com>
2010-09-16 10:44:19 +0000

    Adding wrapper class for AddressValidation in Android Market.

---
+++ src/com/android/i18n/addressinput/MarketVerifier.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.i18n.addressinput;
+
+import com.android.i18n.addressinput.testing.AddressDataMapLoader;
+
+/**
+ * Wrapping for used with android market. Use the method
+ * verify(AddressData address, AddressProblems problems) to validate
+ * AddressData.
+ */
+public class MarketVerifier extends StandardAddressVerifier {
+
+  public MarketVerifier() {
+    super(new FieldVerifierFactory(new AddressVerificationData(
+                                   AddressDataMapLoader.DATA)).rootVerifier());
+  }
+}
