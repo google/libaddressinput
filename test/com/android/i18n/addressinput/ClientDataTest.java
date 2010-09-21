86725d3759658f19f203556cf5eae29db64c6a39
Lara Rennie <lararennie@google.com>
2010-09-21 09:08:20 +0000

    Fixing calls to new CacheData() now it is private.

--- test/com/android/i18n/addressinput/ClientDataTest.java
+++ test/com/android/i18n/addressinput/ClientDataTest.java
@@ -20,7 +20,7 @@ public class ClientDataTest extends AsyncTestCase {
   private ClientData client;
 
   public void setUp() {
-    client = new ClientData(new RegionDataConstants(), new CacheData());
+    client = new ClientData(new RegionDataConstants(), CacheData.getInstance());
   }
 
   public void testPrefetchCountry() {
