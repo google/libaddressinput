6ee62398f25f3ca096d0196b53e55d63606ce65e
Fredrik Roubert <roubert@google.com>
2014-03-20 15:45:29 +0000

    Add a class NullStorage, to use when no storage is required.
    It is not always desirable to cache libaddressinput data. Sometimes it
    might give better performance characteristics to not cache. (Typically
    in tests or when running the data server locally.) This implementation
    of the Storage interface therefore doesn't actually store anything.

---
+++ cpp/include/libaddressinput/null_storage.h
@@ -0,0 +1,48 @@
+// Copyright (C) 2014 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// It is not always desirable to cache libaddressinput data. Sometimes it might
+// give better performance characteristics to not cache. This implementation of
+// the Storage interface therefore doesn't actually store anything.
+
+#ifndef I18N_ADDRESSINPUT_NULL_STORAGE_H_
+#define I18N_ADDRESSINPUT_NULL_STORAGE_H_
+
+#include <libaddressinput/storage.h>
+#include <libaddressinput/util/basictypes.h>
+
+#include <string>
+
+namespace i18n {
+namespace addressinput {
+
+class NullStorage : public Storage {
+ public:
+  NullStorage();
+  virtual ~NullStorage();
+
+  // No-op.
+  virtual void Put(const std::string& key, const std::string& data);
+
+  // Always calls the |data_ready| callback function signalling failure.
+  virtual void Get(const std::string& key, const Callback& data_ready) const;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NullStorage);
+};
+
+}  // namespace addressinput
+}  // namespace i18n
+
+#endif  // I18N_ADDRESSINPUT_NULL_STORAGE_H_
---
+++ cpp/test/null_storage_test.cc
@@ -0,0 +1,75 @@
+// Copyright (C) 2014 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <libaddressinput/callback.h>
+#include <libaddressinput/null_storage.h>
+#include <libaddressinput/storage.h>
+#include <libaddressinput/util/basictypes.h>
+#include <libaddressinput/util/scoped_ptr.h>
+
+#include <string>
+
+#include <gtest/gtest.h>
+
+namespace {
+
+using i18n::addressinput::BuildCallback;
+using i18n::addressinput::NullStorage;
+using i18n::addressinput::scoped_ptr;
+using i18n::addressinput::Storage;
+
+class NullStorageTest : public testing::Test {
+ protected:
+  NullStorageTest() {}
+
+  Storage::Callback* BuildCallback() {
+    return ::BuildCallback(this, &NullStorageTest::OnDataReady);
+  }
+
+  NullStorage storage_;
+  bool success_;
+  std::string key_;
+  std::string data_;
+
+  static const char kKey[];
+
+ private:
+  void OnDataReady(bool success,
+                   const std::string& key,
+                   const std::string& data) {
+    success_ = success;
+    key_ = key;
+    data_ = data;
+  }
+
+  DISALLOW_COPY_AND_ASSIGN(NullStorageTest);
+};
+
+const char NullStorageTest::kKey[] = "foo";
+
+TEST_F(NullStorageTest, Put) {
+  // The Put() method should not do anything, so this test only tests that the
+  // code compiles and that the call doesn't crash.
+  storage_.Put(kKey, "bar");
+}
+
+TEST_F(NullStorageTest, Get) {
+  const scoped_ptr<const Storage::Callback> callback(BuildCallback());
+  storage_.Get(kKey, *callback);
+  EXPECT_FALSE(success_);
+  EXPECT_EQ(kKey, key_);
+  EXPECT_TRUE(data_.empty());
+}
+
+}  // namespace
